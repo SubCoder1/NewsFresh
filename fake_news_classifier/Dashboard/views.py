598600e461e77be45fdd6a289bc924d9f9d6b770
SubCoder1 <ghoshsoumakanti@gmail.com>
2020-02-03 17:20:35 +0530

    added code to handle downvotes from clients

--- fake_news_classifier/Dashboard/views.py
+++ fake_news_classifier/Dashboard/views.py
@@ -2,7 +2,20 @@ from django.shortcuts import render, HttpResponse
 from django.db import transaction
 from django.db.models import F
 from Dashboard.models import NewsModel
-import json
+import json, re, os, pickle
+import pandas as pd
+import numpy as np
+from newspaper import Article
+
+bow = pickle.load(open("./fake_news_classifier/bow.pkl", "rb"))
+model = pickle.load(open("./fake_news_classifier/model.pkl", "rb"))
+
+# Used by bow pickle file
+def clean_article(article):
+    art = re.sub("[^A-Za-z0-9' ]", '', str(article))
+    art2 = re.sub("[( ' )(' )( ')]", ' ', str(art))
+    art3 = re.sub("\s[A-Za-z]\s", ' ', str(art2))
+    return art3.lower()
 
 # Create your views here.
 def DashboardView(request):
@@ -18,8 +31,29 @@ def DashboardView(request):
                 news_vote_obj.save()
                 result = 'valid'
         elif activity == 'downvote':
+            with transaction.atomic():
+                news_model_obj = NewsModel.objects.filter(news_id=news_id).select_for_update().first()
+                news_vote_obj = news_model_obj.news_conn
+                news_vote_obj.downvote_count = F('downvote_count') + 1
+                news_vote_obj.save()
+                result = 'valid'
             # to be coded
-            pass
+            """list_comment = [comment]
+
+            list_comment = clean_article(list_comment)
+            list_comment = [list_comment]
+            vect = bow.transform(list_comment)
+
+            vect = pd.DataFrame(vect.toarray())
+            vect.columns = bow.get_feature_names()
+
+            prediction_array = model.predict(vect)
+            proba_array = model.predict_proba(vect)
+
+            maxProba = np.amax(proba_array)
+            maxProba = format(maxProba, ".2%")
+
+            print(maxProba)"""
         return HttpResponse(json.dumps(result), content_type="application/json")
     news = NewsModel.objects.select_related('news_conn')
     return render(request, 'dashboard.html', {'news':news})
