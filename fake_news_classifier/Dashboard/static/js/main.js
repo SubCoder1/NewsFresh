6a3b9520b7061c4d369749839a4654dcc8c1cb2e
SubCoder1 <ghoshsoumakanti@gmail.com>
2020-02-03 10:12:17 +0530

    added ajax code for upvote & downvote connections

---
+++ fake_news_classifier/Dashboard/static/js/main.js
@@ -0,0 +1,39 @@
+$(document).ready(function() {
+    // JS Function to accquire the csrftoken
+    function getCookie(name) {
+        var cookieValue = null;
+        if (document.cookie && document.cookie !== '') {
+            var cookies = document.cookie.split(';');
+            for (var i = 0; i < cookies.length; i++) {
+                var cookie = cookies[i].trim();
+                if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                    cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                    break;
+                }
+            }
+        }
+        return cookieValue;
+    }
+    var csrftoken = getCookie('csrftoken');
+
+    $('.fa-smile').on('click', function(event) {
+        event.preventDefault();
+        var $upvote = $(this);
+        var news_id = $(this).parent().attr('id');
+        $.ajax({
+            url : '',
+            type : 'POST',
+            data : {
+                csrfmiddlewaretoken : csrftoken,
+                activity : 'upvote',
+                news_id : news_id,
+            },
+            complete : function(response) {
+                if (response.responseJSON == 'valid') {
+                    var upvote_count = parseInt($upvote.siblings('.upvote').text(), 10) + 1;
+                    $upvote.siblings('.upvote').text(upvote_count.toString());
+                }
+            }
+        });
+    });
+});
