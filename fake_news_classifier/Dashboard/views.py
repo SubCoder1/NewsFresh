834f6795f14350bce7d68d30730cf7daad591fb6
SubCoder1 <ghoshsoumakanti@gmail.com>
2020-02-03 10:11:03 +0530

    updated DashboardVeiw to handle upvotes & downvotes

--- fake_news_classifier/Dashboard/views.py
+++ fake_news_classifier/Dashboard/views.py
@@ -1,7 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from django.db import transaction
+from django.db.models import F
 from Dashboard.models import NewsModel
+import json
 
 # Create your views here.
 def DashboardView(request):
+    if request.is_ajax():
+        activity = request.POST.get('activity')
+        news_id = request.POST.get('news_id')
+        result = None
+        if activity == 'upvote':
+            with transaction.atomic():
+                news_model_obj = NewsModel.objects.filter(news_id=news_id).select_for_update().first()
+                news_vote_obj = news_model_obj.news_conn
+                news_vote_obj.upvote_count = F('upvote_count') + 1
+                news_vote_obj.save()
+                result = 'valid'
+        elif activity == 'downvote':
+            # to be coded
+            pass
+        return HttpResponse(json.dumps(result), content_type="application/json")
     news = NewsModel.objects.select_related('news_conn')
     return render(request, 'dashboard.html', {'news':news})
