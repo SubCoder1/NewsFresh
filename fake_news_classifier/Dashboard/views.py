557dfd792e14c7774eebb5fc1b8180e546ed6953
SubCoder1 <ghoshsoumakanti@gmail.com>
2020-02-06 12:58:04 +0530

    added stan's algorithm

--- fake_news_classifier/Dashboard/views.py
+++ fake_news_classifier/Dashboard/views.py
@@ -18,6 +18,43 @@ def clean_article(article):
     art3 = re.sub("\s[A-Za-z]\s", ' ', str(art2))
     return art3.lower()
 
+def classify(news_id):
+    news_model_obj = NewsModel.objects.filter(news_id=news_id).first()
+    news_vote_obj = news_model_obj.news_conn
+    list_comment = [news_model_obj.news]
+
+    list_comment = clean_article(list_comment)
+    list_comment = [list_comment]
+    vect = bow.transform(list_comment)
+
+    vect = pd.DataFrame(vect.toarray())
+    vect.columns = bow.get_feature_names()
+
+    prediction_array = model.predict(vect)
+    proba_array = model.predict_proba(vect)
+
+    maxProba = np.amax(proba_array)
+    
+    total_votes = (news_vote_obj.upvote_count + news_vote_obj.downvote_count)
+    vote_ratio_1, vote_ratio_2 = None, None
+    if total_votes:
+        if news_vote_obj.upvote_count > news_vote_obj.downvote_count:
+            vote_ratio_1 = (news_vote_obj.upvote_count / total_votes) * 100
+            maxProba = (maxProba + vote_ratio_1) / 2
+            print(vote_ratio_1)
+        else:
+            vote_ratio_2 = (news_vote_obj.downvote_count / total_votes) * 100
+            maxProba = (maxProba + vote_ratio_2) / 2
+            print(vote_ratio_2)
+    if 0 in prediction_array:
+        #print("this story is real")
+        news_model_obj.fake = False
+    else:
+        #print("this story is fake")
+        news_model_obj.fake = True
+                
+    news_model_obj.accuracy = float(str("{0:.2f}".format(maxProba)).replace('%', ''))
+    news_model_obj.save()
 # Create your views here.
 def DashboardView(request):
     if request.POST.get('news_link', None) is not None:
@@ -32,6 +69,7 @@ def DashboardView(request):
                     news_vote_model = NewsVoteModel.objects.create()
                     news_model = NewsModel.objects.create(news_link=url, news=article.text, 
                     news_img_link=article.top_image, news_conn=news_vote_model)
+                    classify(news_id=news_model.news_id)
                 else:
                     print("the article could not be scraped")
             else:
@@ -47,44 +85,17 @@ def DashboardView(request):
                 news_model_obj = NewsModel.objects.filter(news_id=news_id).select_for_update().first()
                 news_vote_obj = news_model_obj.news_conn
                 news_vote_obj.upvote_count = F('upvote_count') + 1
-                news_vote_obj.save()
                 result = 'valid'
         elif activity == 'downvote':
             with transaction.atomic():
                 news_model_obj = NewsModel.objects.filter(news_id=news_id).select_for_update().first()
                 news_vote_obj = news_model_obj.news_conn
                 news_vote_obj.downvote_count = F('downvote_count') + 1
-                news_vote_obj.save()
                 result = 'valid'
-
-            news_model_obj = NewsModel.objects.filter(news_id=news_id).first()
-            news_vote_obj = news_model_obj.news_conn
-            if news_vote_obj.downvote_count > 3:
-                list_comment = [news_model_obj.news]
-
-                list_comment = clean_article(list_comment)
-                list_comment = [list_comment]
-                vect = bow.transform(list_comment)
-
-                vect = pd.DataFrame(vect.toarray())
-                vect.columns = bow.get_feature_names()
-
-                prediction_array = model.predict(vect)
-                proba_array = model.predict_proba(vect)
-
-                maxProba = np.amax(proba_array)
-                maxProba = format(maxProba, ".2%")
-
-                print(maxProba)
-                if 0 in prediction_array:
-                    print("this story is real")
-                    news_model_obj.fake = False
-                else:
-                    print("this story is fake")
-                    news_model_obj.fake = True
-                
-                news_model_obj.accuracy = float(str(maxProba).replace('%', ''))
-                news_model_obj.save()
+        
+        news_vote_obj.save()
+        news_model_obj = NewsModel.objects.filter(news_id=news_id).first()
+        classify(news_id=news_model_obj.news_id)
         return HttpResponse(json.dumps(result), content_type="application/json")
     news = NewsModel.objects.select_related('news_conn')
     return render(request, 'dashboard.html', {'news':news})
